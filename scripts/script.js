1cc48e742db778d8dc93ee48341a9a70070610b2
Unknowncreator27 <steel27123@gmail.com>
2025-04-13 00:05:07 +0200

    - Added something fun  - some optimisation  - js  - css

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,18 @@ document.addEventListener("DOMContentLoaded", () => {
     const minutesText = document.getElementById("minutes");
     const secondsText = document.getElementById("seconds");
     const noodleLogo = document.getElementById("noodle-logo");
+    const toggle = document.getElementById("bgToggle");
+
+    const noodleAssets = [
+        // 'res/noodles-takeout.png',
+        // 'res/noodle-pasta-long.png',
+        'res/chopsticks-no-bg.png',
+        // 'res/chinese_takeout_container.png',
+        // 'res/chinese_takeout_w_chopsticks.png'
+        // Add More
+    ];
+
+    let animationRunning = false;
 
     // start date of the event
     const eventDate = new Date("2025-07-25T13:15:00").getTime();
@@ -39,4 +51,51 @@ document.addEventListener("DOMContentLoaded", () => {
     function goToMain(){
         window.location.replace("./main.html");
     }
+
+    
+
+    //Test code
+
+    function checkIfAllItemsFell() {
+        const checker = setInterval(() => {
+            const remaining = document.querySelectorAll('.falling-item');
+            if(remaining.length === 0){
+                clearInterval(checker)
+            }
+        }, 500);
+    }
+    function createFallingObjects(){
+        if(!animationRunning) return;
+
+        const obj = document.createElement("img");
+        obj.src = noodleAssets[Math.floor(Math.random() * noodleAssets.length)];
+        obj.className = "falling-item";
+        obj.style.left = Math.random() * window.innerWidth + "px";
+        obj.style.animationDuration = 3 + Math.random() * 5 + "s";
+        document.body.appendChild(obj);
+
+        setTimeout(() => obj.remove(), 10000); // removes after 10 seconds
+    }
+
+    let fallInterval = null;
+    toggle.addEventListener("change", () => {
+        if(toggle.checked){
+
+            animationRunning = true;
+            fallInterval = setInterval(createFallingObjects, 300);
+            localStorage.setItem('fallingNoodles', 'on');
+        } else {
+            animationRunning = false;
+            clearInterval(fallInterval);
+            checkIfAllItemsFell();
+            localStorage.setItem('fallingNoodles', 'off');
+        }
+    });
+
+    if(localStorage.getItem('fallingNoodles') === 'on'){
+        toggle.checked = true;
+        animationRunning = true;
+        fallInterval = setInterval(createFallingObjects, 300);
+    }
+    // End Of test code
 });
